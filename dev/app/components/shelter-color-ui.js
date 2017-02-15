49a3aa38313b12f84883baba7292e761b71d71db
etiks3 <sylvain.rojas@gmail.com>
2017-02-15 16:21:23 +0100

    setup display

--- dev/app/components/shelter-color-ui.js
+++ dev/app/components/shelter-color-ui.js
@@ -1,6 +1,6 @@
 export function colorSkeleton(){
   return `
-<div id="dataColor" class="gen">
+<div id="dataColor" >
   <div class="card" id="pillar">
     <img src="./img/mat/pillar.png" style="width:100%">
         <h4><b>Couleur 1</b></h4>
@@ -9,12 +9,23 @@ export function colorSkeleton(){
     <img src=""style="width:100%">
       <h4><b>couleur 2</b></h4>
   </div>
-
-</div>
-
-
-
-
-
   `
+}
+
+export function fabricSkeleton(){
+  return `
+  </div>
+  <div id="dataFabric">
+    <div class="card" id="pillar">
+      <img src="./img/mat/pillar.png" style="width:100%">
+          <h4><b>Couleur 1</b></h4>
+    </div>
+    <div class="card">
+      <img src=""style="width:100%">
+        <h4><b>couleur 2</b></h4>
+    </div>
+
+  </div>
+
+    `
 }
