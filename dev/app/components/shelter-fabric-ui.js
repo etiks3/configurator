77de7b5a142eeafabf79db7c3ea7ede381cc8232
etiks3 <sylvain.rojas@gmail.com>
2017-02-15 16:23:12 +0100

    create shelter-fabric-ui.js

---
+++ dev/app/components/shelter-fabric-ui.js
@@ -0,0 +1,16 @@
+export function fabricSkeleton(){
+  return `
+  <div id="dataFabric">
+    <div class="card" id="pillar">
+      <img src="./img/mat/pillar.png" style="width:100%">
+          <h4><b>Couleur 1</b></h4>
+    </div>
+    <div class="card">
+      <img src=""style="width:100%">
+        <h4><b>couleur 2</b></h4>
+    </div>
+
+  </div>
+
+    `
+}
