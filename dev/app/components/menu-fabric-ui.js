319dd57f9b408bcaec31268cce8311ad9392cc65
etiks3 <sylvain.rojas@gmail.com>
2017-02-15 18:43:55 +0100

    up components

---
+++ dev/app/components/menu-fabric-ui.js
@@ -0,0 +1,19 @@
+export function fabricSkeleton(){
+  return `
+  <div id="dataFabric">
+    <div class="card" id="pillar">
+      <img  style="width:100%">
+          <h4><b>Materiaux 1</b></h4>
+    </div>
+    <div class="card">
+      <img src=""style="width:100%">
+        <h4><b>Materiaux 2</b></h4>
+    </div>
+
+  </div>
+
+  <div id="afficher">
+  </div>
+
+    `
+}
