565cea238c5ba5fd6643611333d87bb5a094985a
Sylvain Rojas <syvain.rojas@gmail.com>
2017-02-16 23:34:35 +0100

    add class menu

--- dev/app/components/color-menu-ui.js
+++ dev/app/components/color-menu-ui.js
@@ -1,13 +1,13 @@
 export function colorMenuSkeleton(){
   return `
-<div id="dataColor" >
+<div id="dataColor" class="menu">
   <div class="card" id="cardColor1">
     <img src="./img/mat/pillar.png" style="width:100%">
-    <h4>Couleur 1</h4>
+    <p>Couleur 1</p>
   </div>
   <div class="card" id="cardColor2">
     <img src="./img/mat/Hair.png" style="width:100%">
-    <h4>couleur 2</h4>
+    <p>couleur 2</p>
   </div>
 </div>
   `
