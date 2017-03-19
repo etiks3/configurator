2bdd492bdca90c61c15ff36aa62ec3efcaf47132
Sylvain Rojas <syvain.rojas@gmail.com>
2017-03-19 20:33:16 +0100

    recup firebase

--- dev/app/providers/firebase/firebase-service.js
+++ dev/app/providers/firebase/firebase-service.js
@@ -47,43 +47,6 @@ export class FirebaseService{
     collection = `${collection}/`;
     return this.database.ref(collection).child(key).remove();
   }
-  initApp() {
-        // Listening for auth state changes.
-        // [START authstatelistener]
-        firebase.auth().onAuthStateChanged(function(user) {
-          if (user) {
-            // User is signed in.
-            var displayName = user.displayName;
-            var email = user.email;
-            var emailVerified = user.emailVerified;
-            var photoURL = user.photoURL;
-            var isAnonymous = user.isAnonymous;
-            var uid = user.uid;
-            var providerData = user.providerData;
-            // [START_EXCLUDE]
-            document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-            document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-            document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-            // [END_EXCLUDE]
-          } else {
-            // User is signed out.
-            // [START_EXCLUDE]
-            document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-            document.getElementById('quickstart-sign-in').textContent = 'Sign in with Google';
-            document.getElementById('quickstart-account-details').textContent = 'null';
-            document.getElementById('quickstart-oauthtoken').textContent = 'null';
-            // [END_EXCLUDE]
-          }
-          // [START_EXCLUDE]
-          document.getElementById('quickstart-sign-in').disabled = false;
-          // [END_EXCLUDE]
-        });
-        // [END authstatelistener]
-        document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
-      }
-      // window.onload = function() {
-      //   initApp();
-      // };
-    
+
 
 }
