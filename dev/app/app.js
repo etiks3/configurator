346076c650ae514d523977cb0fea7d21b1419788
Sylvain Rojas <syvain.rojas@gmail.com>
2017-03-26 14:32:22 +0200

    checkout

--- dev/app/app.js
+++ dev/app/app.js
@@ -1,33 +1,33 @@
 /**
-* @Author: Sylvain Rojas <Rojas>
-* @Date:   16-02-2017
-* @Project: Configurator
-* @Filename: app.js
-* @Last modified by:   sylvain
-* @Last modified time: 2017-03-19T20:02:34+01:00
-* @Copyright: S.Rojas
-*/
-
+ * @Author: Sylvain Rojas <Rojas>
+ * @Date:   16-02-2017
+ * @Project: Configurator
+ * @Filename: app.js
+ * @Last modified by:   sylvain
+ * @Last modified time: 2017-03-19T20:02:34+01:00
+ * @Copyright: S.Rojas
+ */
 
 
 import * as firebase from "firebase";
 import { config } from './providers/firebase/config';
 import { HomePage } from './pages/home/home';
 
- export class MyApp {
+export class MyApp {
 
-  constructor(){
+    constructor() {
 
-    //Initialize Firebase
-    firebase.initializeApp(config);
-    this.appBody = document.getElementsByTagName("app")[0];
+        //Initialize Firebase
+        firebase.initializeApp(config);
+        this.appBody = document.getElementsByTagName("app")[0];
 
-  }
-  // Check if user is logged on and prepare to launch the app
-  start(){
-    let homePage = new HomePage(this.appBody);
+    }
 
-        firebase.auth().onAuthStateChanged((user) =>{
+    // Check if user is logged on and prepare to launch the app
+    start() {
+        let homePage = new HomePage(this.appBody);
+
+        firebase.auth().onAuthStateChanged((user) => {
             if (user) {
                 // User is signed in.
                 console.log('User IS log-> ', user)
@@ -45,8 +45,7 @@ import { HomePage } from './pages/home/home';
         });
     }
 
- }
-      
+}
 
 
 //Launch the app
