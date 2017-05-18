b89af2bb4e47708bfe742338ab5ecfc9935de5d6
etiks3 <sylvain.rojas@gmail.com>
2017-05-18 22:36:46 +0200

    upd ui and menu

--- dev/app/pages/home/home-ui.js
+++ dev/app/pages/home/home-ui.js
@@ -1,10 +1,13 @@
 /** * @Author: Sylvain Rojas
 <Rojas>
   * @Date: 16-02-2017 * @Project: Configurator * @Filename: home-ui.js * @Last modified by: sylvain * @Last modified time:
-  2017-03-26T18:20:18+02:00 * @Copyright: S.Rojas */ export function homeSkeleton() { return `
+  2017-03-26T18:20:18+02:00 * @Copyright: S.Rojas */ 
+  
+  
+  export function homeSkeleton() { return `
 
-  <!-- HEADER -->
-
+  <!-- SIDEBAR-->
+ <div class="wrapper">
   <div class="row">
     <ul id="slide-out" class="side-nav">
           <li>
@@ -16,33 +19,35 @@
             <a id="modal-btn-save" data-target="modal1" class="btn modal-trigger">Sauvegarder</a>
           </li>
           <li><a id="logout-btn" class="waves-effect waves-light btn">Logout</a></li>
+          <li>
+            <div id="selectionUser"></div>
+          </li>
     </ul>
     <a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a> 
   </div>
-  <div class="row">
-<!-- MAIN  -->
+  
+
+    <!-- MAIN  -->
+   
     <section id="shelter">
-      <div id="userInfo"></div>
-      <p>Pour configurer votre abris,
-        <br/> veuillez appuyer sur le marqueur vert en bas à gauche de l'écran
-        <p>
-          <div class="choice1 ">
-            <button id="foundation-marker" class="marker1 btn-floating btn-large"><i class="material-icons">add</i></button>
+    <div id="start"></div>
+          <div class="choice1">
+            <button id="foundation-marker" class="marker1 btn-floating btn-large red" ><i class="material-icons">add</i></button>
             <div id="foundations"></div>
           </div>
-          <div class="choice2 ">
+          <div class="choice2">
             <button id="structure-marker" class="marker2 btn-floating btn-large red"><i class="material-icons">add</i></button>
             <div id="structure"></div>
           </div>
-          <div class="choice3 ">
+          <div class="choice3">
             <button id="fabric-marker" class="marker3 btn-floating btn-large red"><i class="material-icons">add</i></button>
             <div id="fabric"></div>
           </div>
-          <div class="choice4 ">
+          <div class="choice4">
             <button id="frontface-marker" class="marker4 btn-floating btn-large red"><i class="material-icons">add</i></button>
             <div id="frontface"></div>
           </div>
-          <div class="choice5 ">
+          <div class="choice5">
             <button id="door-marker" class="marker5 btn-floating btn-large red"><i class="material-icons">add</i></button>
             <div id="door"></div>
           </div>
@@ -50,18 +55,15 @@
             <button id="color-marker" class="marker6 btn-floating btn-large red"><i class="material-icons">add</i></button>
             <div id="color"></div>
           </div>
-          <div id="selectionUser"></div>
+          
           <div class="clear"></div>
-          <div id="selectionUser"></div>
           <div id="display" class="valign-wrapper"></div>
           <a id="reset" class="waves-effect waves-light btn">Effacer</a>
     </section>
+</div>
 
-  </div>
-
-
-  <!-- MODAL -->
-  <div class="nav-wrapper">
+    <!-- MODAL -->
+ 
     <div id="modal1" class="modal modal-fixed-footer">
       <div class="modal-content">
         <h4>Sauvegarder Votre projet</h4>
@@ -70,52 +72,7 @@
       <div class="modal-footer">
         <a href="#!" id="save-btn" class="modal-action modal-close waves-effect waves-green btn-flat">Google Login</a>
       </div>
-    </div>
-
-
-    <!-- MAIN  -->
-    <section id="shelter">
-      <div id="userInfo"></div>
-      <p>Pour configurer votre abris,
-        <br/> veuillez appuyer sur le marqueur vert en bas à gauche de l'écran
-        <p>
-          <div class="choice1 ">
-            <button id="foundation-marker" class="marker1 btn-floating btn-large red"><i class="material-icons">add</i></button>
-            <div id="foundations"></div>
-          </div>
-          <div class="choice2 ">
-            <button id="structure-marker" class="marker2 btn-floating btn-large red"><i class="material-icons">add</i></button>
-            <div id="structure"></div>
-          </div>
-          <div class="choice3 ">
-            <button id="fabric-marker" class="marker3 btn-floating btn-large red"><i class="material-icons">add</i></button>
-            <div id="fabric"></div>
-          </div>
-          <div class="choice4 ">
-            <button id="frontface-marker" class="marker4 btn-floating btn-large red"><i class="material-icons">add</i></button>
-            <div id="frontface"></div>
-          </div>
-          <div class="choice5 ">
-            <button id="door-marker" class="marker5 btn-floating btn-large red"><i class="material-icons">add</i></button>
-            <div id="door"></div>
-          </div>
-          <div class="choice6 ">
-            <button id="color-marker" class="marker6 btn-floating btn-large red"><i class="material-icons">add</i></button>
-            <div id="color"></div>
-          </div>
-          <div id="selectionUser"></div>
-          <div class="clear"></div>
-          <div id="selectionUser"></div>
-          <div id="display" class="valign-wrapper"></div>
-          <a id="reset" class="waves-effect waves-light btn">Effacer</a>
-    </section>
-
-    <!-- FOOTER -->
-    <footer class="page-footer">
-      <div class="container">
-        © 2014 Kalpana-Solutions
-      </div>
-    </footer>
-  </div>
 
   ` }
+
+  
--- dev/app/pages/home/home.js
+++ dev/app/pages/home/home.js
@@ -24,149 +24,135 @@ export class HomePage {
         this.firebaseService = new FirebaseService();
     }
 
-
     //HomePage Initialization
     initUI() {
         // remove all section before display UI
         if (document.getElementsByTagName("section")[0]) {
-            document.getElementsByTagName("section")[0].parentNode.removeChild(document.getElementsByTagName("section")[0])
+            document.getElementsByTagName("section")[0].parentNode
+            .removeChild(document.getElementsByTagName("section")[0])
         }
         // create page skeleton
         let pageSkeleton = homeSkeleton();
         // add page skeleton in body
-        this.appBody.insertAdjacentHTML('afterbegin', pageSkeleton)
+        this.appBody.insertAdjacentHTML('afterbegin', pageSkeleton);
+        document.getElementById('save-btn').addEventListener('click', () => {
+            this.firebaseService.loginWithGoogle();
+        });
+        document.getElementById('logout-btn').addEventListener('click', () => {
+            this.firebaseService.logout();
+            document.getElementById('user').innerHTML = " ";
+            alert("You are leaving?")
+        });
     }
 
     //Event Method Group
     loadEventUI() {
-            let foundation_marker = document.getElementById("foundation-marker")
-            if (foundation_marker) {
-                foundation_marker.addEventListener('click', (e) => {
-                    document.getElementById('start').style.visibility = "hidden";
-                    let menu_foundation = this.shelter.foundationDisplay()
-                    let foundation = document.getElementById("foundations")
-                    foundation.innerHTML = menu_foundation;
-                    this.shelter.dataFoundEvent()
-                });
-            }
-            //A Function to display an item menu on click
-            let structure_marker = document.getElementById("structure-marker");
-            if (structure_marker) {
-                structure_marker.addEventListener('click', (e) => {
-                    let menu_structure = this.shelter.structureDisplay()
-                    let structure = document.getElementById("structure")
-                    structure.innerHTML = menu_structure;
-                    //toggle pour l'affichage du menu
-                    if (structure.style.visibility === 'visible') {
-                        structure.style.visibility = 'hidden';
-                    } else {
-                        structure.style.visibility = 'visible';
-                    }
-                    this.shelter.dataStructureEvent()
-                });
-            }
-            //A Function to display an item menu on click
-            let fabric_marker = document.getElementById("fabric-marker");
-            if (fabric_marker) {
-                fabric_marker.addEventListener('click', (e) => {
-                    let menu_fabric = this.shelter.fabricDisplay()
-                    let fabric = document.getElementById("fabric")
-                    fabric.innerHTML = menu_fabric;
-                    //toggle pour l'affichage du menu
-                    if (fabric.style.visibility === 'visible') {
-                        fabric.style.visibility = 'hidden';
-                    } else {
-                        fabric.style.visibility = 'visible';
-                    }
-                    this.shelter.dataFabricEvent();
-                });
-            }
-
-            //A Function to display an item menu on click
-            let color_marker = document.getElementById("color-marker");
-            if (color_marker) {
-                color_marker.addEventListener('click', (e) => {
-                    let menu_color = this.shelter.colorDisplay()
-                    let color = document.getElementById("color")
-                    color.innerHTML = menu_color;
-                    //toggle pour l'affichage du menu
-                    if (color.style.visibility === 'visible') {
-                        color.style.visibility = 'hidden';
-                    } else {
-                        color.style.visibility = 'visible';
-                    }
-                    this.shelter.dataColorEvent()
-                });
-            }
-
-            let door_marker = document.getElementById("door-marker");
-            if (door_marker) {
-                door_marker.addEventListener('click', (e) => {
-                    let menu_door = this.shelter.doorDisplay()
-                    let door = document.getElementById("door")
-                    door.innerHTML = menu_door;
-                    //toggle pour l'affichage du menu
-                    if (door.style.visibility === 'visible') {
-                        door.style.visibility = 'hidden';
-                    } else {
-                        door.style.visibility = 'visible';
-                    }
-                    this.shelter.dataDoorEvent()
-                });
-            }
-            let frontface_marker = document.getElementById("frontface-marker");
-            if (frontface_marker) {
-                frontface_marker.addEventListener('click', (e) => {
-                    let menu_frontface = this.shelter.frontfaceDisplay()
-                    let frontface = document.getElementById("frontface")
-                    frontface.innerHTML = menu_frontface;
-                    //toggle pour l'affichage du menu
-                    if (frontface.style.visibility === 'visible') {
-                        frontface.style.visibility = 'hidden';
-                    } else {
-                        frontface.style.visibility = 'visible';
-                    }
-                    this.shelter.datafrontfaceEvent()
-                });
-            }
-            let roof_marker = document.getElementById("roof-marker");
-            if (roof_marker) {
-                roof_marker.addEventListener('click', (e) => {
-                    let menu_roof = this.shelter.roofDisplay()
-                    let roof = document.getElementById("roof")
-                    roof.innerHTML = menu_roof;
-                    //toggle pour l'affichage du menu
-                    if (roof.style.visibility === 'visible') {
-                        roof.style.visibility = 'hidden';
-                    } else {
-                        roof.style.visibility = 'visible';
-                    }
-                    this.shelter.dataRoofEvent()
-                });
-            }
-            document.getElementById('save-btn').addEventListener('click', () => {
-                this.firebaseService.loginWithGoogle();
-            });
-            document.getElementById('logout-btn').addEventListener('click', () => {
-                    this.firebaseService.logout();
-                    document.getElementById('user').innerHTML=" ";
-                    alert("You are leaving?")
-                     });
+        let foundation_marker = document.getElementById("foundation-marker");   
+        if (foundation_marker) {
+           foundation_marker.addEventListener('click', (e) => {
+               console.log(e)
+                document.getElementById('start').style.visibility = "hidden";
+                let menu_foundation = this.shelter.foundationDisplay()
+                let foundation = document.getElementById("foundations")
+                foundation.innerHTML = menu_foundation;
+                this.shelter.dataFoundEvent()
+        });
         }
-        // Method to save all projects data in Firebase and link user to database
-        // saveData() {
-        //   if (this.firebaseService.isLogged()) {
-        //     document.getElementById('logout-btn').innerHTML = '';
-        //     console.log('hello')
-        //     this.firebaseService.create(this.shelter.userSelection)
-        //       .then(
-        //         (response)=>{
-        //           console.log(response);
-        //         }
-        //       ).catch(
-        //       err => console.log(err)
-        //     )
-        //   }
-        // }
+        //A Function to display an item menu on click
+        let structure_marker = document.getElementById("structure-marker");
+        if (structure_marker) {
+            structure_marker.addEventListener('click', (e) => {
+                let menu_structure = this.shelter.structureDisplay()
+                let structure = document.getElementById("structure")
+                structure.innerHTML = menu_structure;
+                //toggle pour l'affichage du menu
+                if (structure.style.visibility === 'visible') {
+                    structure.style.visibility = 'hidden';
+                } else {
+                    structure.style.visibility = 'visible';
+                }
+                this.shelter.dataStructureEvent()
+            });
+        }
+        
+        //A Function to display an item menu on click
+        let fabric_marker = document.getElementById("fabric-marker");
+        if (fabric_marker) {
+            fabric_marker.addEventListener('click', (e) => {
+                let menu_fabric = this.shelter.fabricDisplay()
+                let fabric = document.getElementById("fabric")
+                fabric.innerHTML = menu_fabric;
+                //toggle pour l'affichage du menu
+                if (fabric.style.visibility === 'visible') {
+                    fabric.style.visibility = 'hidden';
+                } else {
+                    fabric.style.visibility = 'visible';
+                }
+                this.shelter.dataFabricEvent();
+            });
+        }
+
+        //A Function to display an item menu on click
+        let color_marker = document.getElementById("color-marker");
+        if (color_marker) {
+            color_marker.addEventListener('click', (e) => {
+                let menu_color = this.shelter.colorDisplay()
+                let color = document.getElementById("color")
+                color.innerHTML = menu_color;
+                //toggle pour l'affichage du menu
+                if (color.style.visibility === 'visible') {
+                    color.style.visibility = 'hidden';
+                } else {
+                    color.style.visibility = 'visible';
+                }
+                this.shelter.dataColorEvent()
+            });
+        }
+
+        let door_marker = document.getElementById("door-marker");
+        if (door_marker) {
+            door_marker.addEventListener('click', (e) => {
+                let menu_door = this.shelter.doorDisplay()
+                let door = document.getElementById("door")
+                door.innerHTML = menu_door;
+                //toggle pour l'affichage du menu
+                if (door.style.visibility === 'visible') {
+                    door.style.visibility = 'hidden';
+                } else {
+                    door.style.visibility = 'visible';
+                }
+                this.shelter.dataDoorEvent()
+            });
+        }
+        let frontface_marker = document.getElementById("frontface-marker");
+        if (frontface_marker) {
+            frontface_marker.addEventListener('click', (e) => {
+                let menu_frontface = this.shelter.frontfaceDisplay()
+                let frontface = document.getElementById("frontface")
+                frontface.innerHTML = menu_frontface;
+                //toggle pour l'affichage du menu
+                if (frontface.style.visibility === 'visible') {
+                    frontface.style.visibility = 'hidden';
+                } else {
+                    frontface.style.visibility = 'visible';
+                }
+                this.shelter.datafrontfaceEvent()
+            });
+        }
+        let roof_marker = document.getElementById("roof-marker");
+        if (roof_marker) {
+            roof_marker.addEventListener('click', (e) => {
+                let menu_roof = this.shelter.roofDisplay()
+                let roof = document.getElementById("roof")
+                roof.innerHTML = menu_roof;
+                //toggle pour l'affichage du menu
+                if (roof.style.visibility === 'visible') {
+                    roof.style.visibility = 'hidden';
+                } else {
+                    roof.style.visibility = 'visible';
+                }
+                this.shelter.dataRoofEvent()
+            });
+        }
+    }
 }
-     
